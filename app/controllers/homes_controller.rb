3278a03f9b6be83b99099953ce7a4890e0869d44
Glen Kutsovsky <gkstr1@hotmail.com>
2014-08-13 22:39:28 +0000

    added shouts controller and shout user model

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,21 @@
+class HomesController < ApplicationController
+    before_filter :check_user_logged_in
+    
+    def show
+    end
+   
+    private 
+    
+        def check_user_logged_in
+                if signed_in?
+                redirect_to dashboard_path
+       end
+   end
+    
+        
+        
+
+
+end
+
+
